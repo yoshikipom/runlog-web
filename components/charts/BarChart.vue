557b70a723b3cbddd4b84a21df1f7c700f108c80
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2020-06-28 01:33:20 +0900

    [Add] bar chart class

---
+++ components/charts/BarChart.vue
@@ -0,0 +1,36 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar, 
+  props: {
+    x: {default: []},
+    y: {default: []}
+  },
+   mounted () {
+    this.renderChart({
+      labels: this.x,
+      datasets: [
+        {
+          label: 'Month distance record',
+          backgroundColor: '#f87979',
+          data: this.y
+        }
+      ]
+    },
+    {
+      responsive: true, 
+      maintainAspectRatio: false, 
+      scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              }
+            }
+          ]
+        }
+    })
+  }
+}
+</script>
